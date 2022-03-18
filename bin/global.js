a2daf2f4c20b78493cb3c09a6474dfa04e067fbe
Carlos Rodriguez <rodriguezcarlos.cdr@gmail.com>
2022-03-18 15:41:21 -0300

    Create my first module in npm

---
+++ bin/global.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+import { printNames } from "../src/index";
+
+printNames();
+
+// const print = require('../src/index');
+
+// print.printNames();
---
+++ src/index.js
@@ -0,0 +1,15 @@
+const nameList = [
+    'carlos',
+    'sofia',
+    'mariana',
+    'micaela',
+    'agostina'
+]
+
+const printNames = () =>{
+    for (let oneName of nameList){
+        console.log(oneName);
+    }
+}
+
+export {printNames};
